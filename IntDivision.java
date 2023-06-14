0cebdc6af4a70c3a0431d10b6ac42a7ef83eb549
ArunPandyanVasam <you@example.com>
2023-06-14 18:33:47 -0400

    version

---
+++ IntDivision.java
@@ -0,0 +1,10 @@
+package intDivision;
+
+public class IntDivision {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+
+        System.out.println("x/y: " + (x/y)); // gives output as quotient
+    }
+}
